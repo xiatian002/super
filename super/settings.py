5a426e5a0fa9bb8f94276e5265b1608a4e10645d
xiatian002 <zhang_peng101@126.com>
2018-06-19 23:07:11 +0800

    增加上下文渲染器使每个页面都能显示导航栏

--- super/settings.py
+++ super/settings.py
@@ -71,6 +71,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'news_web.nav_processer.nav_column',
             ],
         },
     },
